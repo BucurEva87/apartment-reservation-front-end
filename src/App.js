4c5fc94e5db66a69305788b73542e9a3b5af774a
BucurEva87 <bucur.eva87@gmail.com>
2023-04-20 17:54:03 +0300

    Replace Login component with HomePage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import NavBar from './components/NavBar';
-import Login from './components/Login';
 import HomePage from './Pages/HomePage';
 import ReservationPage from './Pages/ReservationPage';
 import ReservationDetailsPage from './Pages/ReservationDetailsPage';
