0ef1696baa2840ef9f5d27dd62d5a569cb05bdd0
abdallah <abdallahantony55.aa@gmail.com>
2023-04-21 10:21:34 +0300

    style login

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,9 +24,29 @@ const Login = () => {
 
   return (
     <>
-      <input type="email" name="email" id="email" ref={emailRef} />
-      <input type="password" name="password" id="password" ref={passwordRef} />
-      <button type="button" onClick={handleLogin}>Sign in</button>
+ <div class="flex justify-center items-center h-screen bg-gray-100">
+  <div class="bg-white p-10 rounded-lg shadow-xl transform transition-all w-2/3 max-w-md hover:scale-105">
+    <h1 class="text-2xl font-bold mb-8 text-center">Login</h1>
+    <form class="space-y-5">
+      <div>
+        <label class="block mb-1 font-bold">Email</label>
+        <input type="email" class="block w-full p-2 border rounded-lg" name="email" id="email" ref={emailRef} />
+      </div>
+      <div>
+        <label class="block mb-1 font-bold">Password</label>
+        <input type="password" name="password" id="password" ref={passwordRef} class="block w-full p-2 border rounded-lg" />
+      </div>
+      <div>
+        <button type="button" onClick={handleLogin} class="w-full bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600">Login</button>
+      </div>
+    </form>
+    <hr class="my-6 border-gray-300 w-full" />
+    <div class="text-center">
+      <button type="button" class="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600">Register</button>
+    </div>
+  </div>
+</div>
+
     </>
   );
 };
