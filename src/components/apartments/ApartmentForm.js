a3eaa60f63e63db07beb0eab00dc3f59aba5cf56
kidde60 <kgeorgewilliam60@gmail.com>
2023-04-22 16:17:05 +0300

    Remove linter errors

--- src/components/apartments/ApartmentForm.js
+++ src/components/apartments/ApartmentForm.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { createApartment } from '../../redux/apartments/apartmentsThunk';
@@ -22,16 +22,15 @@ const ApartmentForm = ({ apartmentId }) => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const resetForm = () => {
     setFormData({
-    name: '',
-    city: '',
-    description: '',
-    photo: '',
-    price: 0,
+      name: '',
+      city: '',
+      description: '',
+      photo: '',
+      price: 0,
     });
-    
   };
 
   const handleSubmit = (e) => {
@@ -39,7 +38,7 @@ const ApartmentForm = ({ apartmentId }) => {
 
     dispatch(createApartment({ apartment: formData, accessToken }));
     resetForm();
-    navigate('/apartments')
+    navigate('/apartments');
   };
 
   return (
@@ -54,49 +53,49 @@ const ApartmentForm = ({ apartmentId }) => {
       </div>
       <div>
         <label className="block mb-1 font-bold">Description</label>
-        <input className="block w-full p-2 border rounded-lg" 
-        type="text" 
-        name="description" 
-        value={formData.description} 
-        onChange={handleInputChange} 
-        required 
+        <input className="block w-full p-2 border rounded-lg"
+        type="text"
+        name="description"
+        value={formData.description}
+        onChange={handleInputChange}
+        required
         />
       </div>
 
       <div>
         <label className="block mb-1 font-bold">Photo</label>
-        <input className="block w-full p-2 border rounded-lg" 
-        required 
-        type="text" 
-        name="photo" 
-        value={formData.photo} 
-        onChange={handleInputChange} 
+        <input className="block w-full p-2 border rounded-lg"
+        required
+        type="text"
+        name="photo"
+        value={formData.photo}
+        onChange={handleInputChange}
         />
       </div>
-     
+
       <div>
         <label className="block mb-1 font-bold">City</label>
-        <input className="block w-full p-2 border rounded-lg" 
-        type="text" 
-        name="city" 
-        value={formData.city} 
-        onChange={handleInputChange} 
-        required 
+        <input className="block w-full p-2 border rounded-lg"
+        type="text"
+        name="city"
+        value={formData.city}
+        onChange={handleInputChange}
+        required
         />
       </div>
 
       <div>
         <label className="block mb-1 font-bold">Price</label>
-        <input className="block w-full p-2 border rounded-lg" 
-        required 
-        type="number" 
+        <input className="block w-full p-2 border rounded-lg"
+        required
+        type="number"
         name="price"
-        step={0.01} 
-        value={formData.price} 
-        onChange={handleInputChange} 
+        step={0.01}
+        value={formData.price}
+        onChange={handleInputChange}
         />
       </div>
-     
+
       <div className="text-center">
       <button type='submit' className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-800">Add a new apartment</button>
      </div>
